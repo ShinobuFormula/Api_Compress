74982d17682f460cff9f0c43907b5edecdfa2d20
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-10-31 15:57:41 +0100

    :hammer:  various compress function fix
    -- changing output folder
    -- compliance of the expected input and handling

--- api/compressFiles.js
+++ api/compressFiles.js
@@ -1,27 +1,28 @@
 const fs = require('fs');
 const archiver = require('archiver');
+var path = require("path");
+
 
 function compress(paths, archiveName) {
 
-    var archivePath = __dirname + '\\Folder_Test\\' + archiveName;
-
+    var archivePath = path.join(__dirname, `../fichiers/${archiveName}`)
     var output = fs.createWriteStream(archivePath);
     var archive = archiver('zip');
 
-    output.on('close', function() {
+    output.on('close', function () {
         console.log(archive.pointer() + ' total bytes');
         console.log('archiver has been finalized and the output file descriptor has closed.');
     });
 
-// This event is fired when the data source is drained no matter what was the data source.
-// It is not part of this library but rather from the NodeJS Stream API.
-// @see: https://nodejs.org/api/stream.html#stream_event_end
-    output.on('end', function() {
+    // This event is fired when the data source is drained no matter what was the data source.
+    // It is not part of this library but rather from the NodeJS Stream API.
+    // @see: https://nodejs.org/api/stream.html#stream_event_end
+    output.on('end', function () {
         console.log('Data has been drained');
     });
 
-// good practice to catch warnings (ie stat failures and other non-blocking errors)
-    archive.on('warning', function(err) {
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on('warning', function (err) {
         if (err.code === 'ENOENT') {
             // log warning
         } else {
@@ -30,23 +31,25 @@ function compress(paths, archiveName) {
         }
     });
 
-// good practice to catch this error explicitly
-    archive.on('error', function(err) {
+    // good practice to catch this error explicitly
+    archive.on('error', function (err) {
         throw err;
     });
 
     archive.pipe(output);
 
 
-    paths.forEach((path) => {
-        let fileName = path[1].split('\\');
-        archive.append(fs.createReadStream(path[1]), { name: fileName[fileName.length-1] });
+    let unwindedPaths = [].concat(...paths);
+    unwindedPaths.forEach((path) => {
+        let fileName = path.split('\\');
+        archive.append(fs.createReadStream(path), { name: fileName[fileName.length-1] });
 
     });
 
     archive.finalize();
 
-    return {archivePath}
+    return archivePath
+    
 }
 
 module.exports = {
