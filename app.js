41d97888df82b474ce63be9d02c263b5fcc5c967
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-10-31 21:38:12 +0100

    :hammer: Serving files statically to improve downloading management

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var path = require("path");
+
 
 const getPath = require('./route/getPath');
 
@@ -9,6 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/", getPath);
+app.use('/compressed', express.static(path.join(__dirname,'/fichiers')));
+
 
 app.listen(5000, ()=>{
     console.log("Listening for queries...")
