8fede573436318e5a8c16d61624128b092d6acf9
Patrick-Ivann <33460507+Patrick-Ivann@users.noreply.github.com>
2019-10-31 15:58:23 +0100

    :ahmmer: compliance with the input of the api

--- route/getPath.js
+++ route/getPath.js
@@ -3,18 +3,10 @@ const compress = require('../api/compressFiles').compress;
 
 const router = express.Router();
 
+
 router.post('/?*', (req, res) => {
-    var paths = Object.entries(req.body);
-
-
-    paths.forEach((elem, index) => {
-        if(elem[1] === req.body.archiveName){
-           paths.splice(index,1)
-        }
-    });
-
-
-    res.json(compress(paths,req.body.archiveName));
+    var paths = (req.body.toCompress);
+    res.status(200).send(compress(paths,req.body.archiveName));
 });
 
 
